3bb4698b6ebb1834c2acd8bffaca3c0e19384733
soundreaper@gmail.com <soundreaper@gmail.com>
2021-03-05 08:47:29 +0545

    complete all challenges

--- scrape.go
+++ scrape.go
@@ -1,30 +1,87 @@
 package main
 
 import (
+	"encoding/json"
+	"flag"
 	"fmt"
+	"io/ioutil"
+	"log"
+	"strings"
 
 	"github.com/gocolly/colly"
 )
 
-// main() contains code adapted from example found in Colly's docs:
-// http://go-colly.org/docs/examples/basic/
+type movie struct {
+	Title string
+	Year  string
+}
+
+type star struct {
+	Name      string
+	Photo     string
+	JobTitle  string
+	BirthDate string
+	Bio       string
+	TopMovies []movie
+}
+
 func main() {
-	// Instantiate default collector
-	c := colly.NewCollector()
+	month := flag.Int("month", 1, "Month to fetch birthdays for")
+	day := flag.Int("day", 1, "Day to fetch birthdays for")
+	flag.Parse()
+	crawl(*month, *day)
+}
 
-	// On every a element which has href attribute call callback
-	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
-                link := e.Attr("href")
+func crawl(month int, day int) {
+	c := colly.NewCollector(
+		colly.AllowedDomains("imdb.com", "www.imdb.com"),
+	)
+	infoCollector := c.Clone()
 
-		// Print link
-                fmt.Printf("Link found: %q -> %s\n", e.Text, link)
+	c.OnHTML(".mode-detail", func(e *colly.HTMLElement) {
+		profileURL := e.ChildAttr("div.lister-item-image > a", "href")
+		profileURL = e.Request.AbsoluteURL(profileURL)
+		infoCollector.Visit(profileURL)
 	})
 
-	// Before making a request print "Visiting ..."
+	c.OnHTML("a.lister-page-next", func(e *colly.HTMLElement) {
+		nextPage := e.Request.AbsoluteURL(e.Attr("href"))
+		c.Visit(nextPage)
+	})
+
+	infoCollector.OnHTML("#content-2-wide", func(e *colly.HTMLElement) {
+		tmpProfile := star{}
+		tmpProfile.Name = e.ChildText("h1.header > span.itemprop")
+		tmpProfile.Photo = e.ChildAttr("#name-poster", "src")
+		tmpProfile.JobTitle = e.ChildText("#name-job-categories > a > span.itemprop")
+		tmpProfile.BirthDate = e.ChildAttr("#name-born-info time", "datetime")
+
+		tmpProfile.Bio = strings.TrimSpace(e.ChildText("#name-bio-text > div.name-trivia-bio-text > div.inline"))
+
+		e.ForEach("div.knownfor-title", func(_ int, kf *colly.HTMLElement) {
+			tmpMovie := movie{}
+			tmpMovie.Title = kf.ChildText("div.knownfor-title-role > a.knownfor-ellipsis")
+			tmpMovie.Year = kf.ChildText("div.knownfor-year > span.knownfor-ellipsis")
+			tmpProfile.TopMovies = append(tmpProfile.TopMovies, tmpMovie)
+		})
+
+		js, err := json.MarshalIndent(tmpProfile, "", "    ")
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		ioutil.WriteFile("actors.json", js, 0644)
+	})
+
 	c.OnRequest(func(r *colly.Request) {
-		fmt.Println("Visiting", r.URL.String())
+		fmt.Println("Visiting: ", r.URL.String())
 	})
 
-	// Start scraping on https://hackerspaces.org
-	c.Visit("https://hackerspaces.org/")
+	infoCollector.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting Profile URL: ", r.URL.String())
+	})
+
+	startURL := fmt.Sprintf("https://www.imdb.com/search/name/?birth_monthday=%d-%d", month, day)
+	c.Visit(startURL)
 }
